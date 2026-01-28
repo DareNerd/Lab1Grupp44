3becb15e90f41af84695b00f59fbc5b4e7c5a635
fjelln <198716059+fjelln@users.noreply.github.com>
2026-01-28 20:50:37 +0100

    Other methods

--- Car.java
+++ Car.java
@@ -14,11 +14,84 @@ public abstract class Car {
 
     public Car() {}
 
+    public Car(int nrDoors, double enginePower, Color color, String modelName) {
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.color = color;
+        this.modelName = modelName;
+        stopEngine();
+    }
+
+    public void move() {
+        if(this.direction.equals("north")) {
+            this.y += getCurrentSpeed();
+        } else if (this.direction.equals("south")) {
+            this.y -= getCurrentSpeed();
+        } else if (this.direction.equals("east")) {
+            this.x += getCurrentSpeed();
+        } else {
+            this.x -= getCurrentSpeed();
+        }
+    }
+
+    public void turnLeft() {
+        if(this.direction.equals("north")) {
+            this.direction = "west";
+        } else if (this.direction.equals("south")) {
+            this.direction = "east";
+        } else if (this.direction.equals("east")) {
+            this.direction = "north";
+        } else {
+            this.direction = "south";
+        }
+    }
+
+    public void turnRight() {
+        if(this.direction.equals("north")) {
+            this.direction = "east";
+        } else if (this.direction.equals("south")) {
+            this.direction = "west";
+        } else if (this.direction.equals("east")) {
+            this.direction = "south";
+        } else {
+            this.direction = "north";
+        }
+    }
+
+    public void gas(double amount){
+        if (amount < 0 || amount > 1) {
+            throw new IllegalArgumentException("illegal amount");
+        }
+        incrementSpeed(amount);
+    }
+
+    public void brake(double amount){
+        if (0 <= amount && amount <= 1) {
+            decrementSpeed(amount);
+        } else {
+            throw new IllegalArgumentException("illegal amount");
+        }
+    }
+
+    public abstract double speedFactor();
+
+    public abstract void incrementSpeed(double amount);
+
+    public abstract void decrementSpeed(double amount);
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
     public int getNrDoors() {
         return nrDoors;
     }
 
-    public void setNrDoors(int nrDoors) {
+    protected void setNrDoors(int nrDoors) {
         this.nrDoors = nrDoors;
     }
 
@@ -26,7 +99,7 @@ public abstract class Car {
         return enginePower;
     }
 
-    public void setEnginePower(double enginePower) {
+    protected void setEnginePower(double enginePower) {
         this.enginePower = enginePower;
     }
 
@@ -34,8 +107,13 @@ public abstract class Car {
         return currentSpeed;
     }
 
-    public void setCurrentSpeed(double currentSpeed) {
-        this.currentSpeed = currentSpeed;
+    public void setCurrentSpeed(double speed) {
+        if (speed > this.enginePower) {
+            throw new IllegalArgumentException("speed too high");
+        } else if (speed < 0) {
+            throw new IllegalArgumentException("speed can't be negative");
+        }
+        this.currentSpeed = speed;
     }
 
     public Color getColor() {
@@ -50,7 +128,7 @@ public abstract class Car {
         return modelName;
     }
 
-    public void setModelName(String modelName) {
+    protected void setModelName(String modelName) {
         this.modelName = modelName;
     }
 
