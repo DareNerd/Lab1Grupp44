0b8864f1a1c0561e86d46f4a49f1124f985b5c32
fjelln <198716059+fjelln@users.noreply.github.com>
2026-02-02 14:57:15 +0100

    Truck super class

---
+++ Truck.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+
+public abstract class Truck extends Car {
+    protected double truckBedAngle = 0;
+
+    public Truck(int nrDoors, double enginePower, Color color, String modelName) {
+        super(nrDoors, enginePower, color, modelName);
+    }
+
+
+
+    public void setTruckBedAngle(double angle) {
+        if (this.getCurrentSpeed() != 0) {
+            throw new RuntimeException("you are driving!");
+        } else if (angle < 0 || angle > 70) {
+            throw new IllegalArgumentException("illegal angle");
+        }
+        this.truckBedAngle = angle;
+    }
+
+    public void move() {
+        if (this.truckBedAngle != 0) {
+            throw new RuntimeException("your truck bed is up!");
+        }
+        super.move();
+    }
+}
