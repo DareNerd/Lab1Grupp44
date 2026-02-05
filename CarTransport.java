bfc2b49add4f8fae145c4983f38b210416c0f566
fjelln <198716059+fjelln@users.noreply.github.com>
2026-02-05 20:46:46 +0100

    CarTransport loadCar, unLoadCar, loaded cars positions etc.

--- CarTransport.java
+++ CarTransport.java
@@ -1,6 +1,9 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public class CarTransport extends Truck {
+    private final int MAX_CAPACITY = 8;
+    private final ArrayList<Car> loadedCars = new ArrayList<>();
 
     public CarTransport() {
         super(2, 3000, Color.yellow, "CarTransport");
@@ -8,6 +11,38 @@ public class CarTransport extends Truck {
 
     public double speedFactor() {
         return getEnginePower() * 0.0001;
+    }
+
+    public void loadCar(Car car) {
+        double distance;
+
+        distance = Math.sqrt(Math.pow(this.getX() - car.getX(), 2.0)
+                + Math.pow(this.getY() - car.getY(), 2.0));
+
+        if (loadedCars.size() == MAX_CAPACITY) {
+            System.out.println("Max capacity!");
+        } else if (distance > 5.0) {
+            System.out.println("The car is too far away!");
+        } else {
+            loadedCars.add(car);
+            car.setX(this.getX());
+            car.setY(this.getY());
+        }
+    }
+
+    public void unLoadCar() {
+        Car lastCar = loadedCars.getLast();
+        lastCar.setX(this.getX() - 1);
+        loadedCars.removeLast();
+    }
+
+    public void move() {
+        super.move();
+
+        for (Car car : loadedCars) {
+            car.setX(this.getX());
+            car.setY(this.getY());
+        }
     }
 
     // TODO: fixa truck bed angle så den tar int 0 eller 1
