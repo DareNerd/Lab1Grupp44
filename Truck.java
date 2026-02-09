fa86183560fc5d58ce6bc43ff388c9a8373e120b
fjelln <198716059+fjelln@users.noreply.github.com>
2026-02-09 14:58:17 +0100

    Fixat Truck

--- Truck.java
+++ Truck.java
@@ -6,20 +6,4 @@ public abstract class Truck extends Car {
     public Truck(int nrDoors, double enginePower, Color color, String modelName) {
         super(nrDoors, enginePower, color, modelName);
     }
-
-    public void setTruckBedAngle(double angle) {
-        if (this.getCurrentSpeed() != 0) {
-            throw new RuntimeException("you are driving!");
-        } else if (angle < 0 || angle > 70) {
-            throw new IllegalArgumentException("illegal angle");
-        }
-        this.truckBedAngle = angle;
-    }
-
-    public void move() {
-        if (this.truckBedAngle != 0) {
-            throw new RuntimeException("your truck bed is up!");
-        }
-        super.move();
-    }
 }
