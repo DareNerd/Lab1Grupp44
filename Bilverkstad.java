2258456dcf61bfe94a98298aee59ef064f1d8aae
fjelln <198716059+fjelln@users.noreply.github.com>
2026-02-09 14:57:32 +0100

    Bilverkstad + några tester

--- Bilverkstad.java
+++ Bilverkstad.java
@@ -7,15 +7,25 @@ public class Bilverkstad<A> {
     public Bilverkstad(int capacity) {
         this.maxCapacity = capacity;
     }
-    void addCar(A car) {
-        carArrayList.add(car);
+
+
+    public void addCar(A car) {
+        if (carArrayList.size() >= maxCapacity) {
+            System.out.println("Bilverkstaden är full!");
+        } else {
+            carArrayList.add(car);
+        }
     }
 
-    void removeCar(A car) {
+    public void removeCar(A car) {
         if (!carArrayList.remove(car)) {
             System.out.println("Du har gått till fel bilverkstad!");
         } else {
             carArrayList.remove(car);
         }
+    }
+
+    public int getCapacity() {
+        return carArrayList.size();
     }
 }
