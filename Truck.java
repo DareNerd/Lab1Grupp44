79c01e356bf418a127eb24878a36b83dc39d3dd1
fjelln <198716059+fjelln@users.noreply.github.com>
2026-02-10 11:40:31 +0100

    removed truckBedAngle

--- Truck.java
+++ Truck.java
@@ -1,7 +1,6 @@
 import java.awt.*;
 
 public abstract class Truck extends Car {
-    protected double truckBedAngle = 0;
 
     public Truck(int nrDoors, double enginePower, Color color, String modelName) {
         super(nrDoors, enginePower, color, modelName);
