d1e981163830de36df84621f59c839866ad018ef
fjelln <198716059+fjelln@users.noreply.github.com>
2026-02-26 14:17:58 +0100

    Implemented reverseDirection in Car

--- Car.java
+++ Car.java
@@ -163,4 +163,16 @@ public abstract class Car implements Movable {
     public void setDirection(Directions direction) {
         this.direction = direction;
     }
+
+    public void reverseDirection() {
+        double speed = getCurrentSpeed();
+        setCurrentSpeed(0);
+        switch (this.direction) {
+            case Directions.NORTH: this.direction = Directions.SOUTH; break;
+            case Directions.SOUTH: this.direction = Directions.NORTH; break;
+            case Directions.WEST: this.direction = Directions.EAST; break;
+            case Directions.EAST: this.direction = Directions.WEST; break;
+        }
+        setCurrentSpeed(speed);
+    }
 }
