c6ddc85f86fb9f60d17a331003dbba7f78fb236e
fjelln <198716059+fjelln@users.noreply.github.com>
2026-02-26 14:21:36 +0100

    Tog bort onödig import

--- CarController.java
+++ CarController.java
@@ -3,7 +3,6 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Iterator;
 
 
 /*
